f9a47c11efecd66f96111fb8df806c8b19773fe6
mariosangiorgio <mariosangiorgio@8dedc91c-0c52-11df-bbc3-97105669a493>
2010-04-02 22:37:48 +0000

    The crawler skips already downloaded papers

--- src/applications/Statistics.java
+++ src/applications/Statistics.java
@@ -2,7 +2,11 @@ package applications;
 
 import it.polimi.data.hibernate.HibernateSessionManager;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Vector;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -18,6 +22,10 @@ public class Statistics {
 		int lastYear = 2010;
 		int step = 5;
 
+		HashMap<String, Vector<Float>> table;
+		table = new HashMap<String, Vector<Float>>();
+
+		int i = 0;
 		for (int year = firstYear; year <= lastYear; year += step) {
 			Query query = session.getNamedQuery("getTotalNumerOfPapers");
 
@@ -31,16 +39,51 @@ public class Statistics {
 			query.setParameter("firstYear", year);
 			query.setParameter("lastYear", year + step - 1);
 
-			System.out.println("From " + year + " to " + (year + step - 1));
-
 			List result = query.list();
 			for (Object o : result) {
 				Object[] record = (Object[]) o;
 				String classification = (String) record[0];
 				Long count = (Long) record[1];
-				System.out.println(classification + "," + count + ","
-						+ ((float) count) / total);
+
+				if (table.get(classification) == null) {
+					table.put(classification, new Vector<Float>());
+				}
+				while (table.get(classification).size() < i) {
+					table.get(classification).add(0f);
+				}
+				table.get(classification).add((float) count / total);
 			}
+			i++;
+		}
+		// Filling all the gaps
+		for(String key : table.keySet()){
+			while (table.get(key).size() < i) {
+				table.get(key).add(0f);
+			}
+		}
+		
+		// Writing table to file
+		try {
+			FileWriter file = new FileWriter("table.csv");
+			BufferedWriter buffer = new BufferedWriter(file);
+			buffer.write("topic,");
+			for (int year = firstYear; year <= lastYear; year += step) {
+				buffer.write(year+"-"+(year+step-1)+",");
+			}
+			buffer.write("\n");
+			for (String key : table.keySet()) {
+				buffer.write(key + ",");
+				i = 0;
+				for (i = 0; i < (table.get(key).size() - 1); i++) {
+					buffer.write(table.get(key).get(i) + ",");
+				}
+				buffer.write(table.get(key).get(i) + "\n");
+
+			}
+			buffer.close();
+			file.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		session.close();
 	}
--- src/applications/analyzer/BayesianDocumentClassifier.java
+++ src/applications/analyzer/BayesianDocumentClassifier.java
@@ -4,7 +4,8 @@ import cc.mallet.classify.ClassifierTrainer;
 import cc.mallet.classify.NaiveBayesTrainer;
 
 public class BayesianDocumentClassifier extends DocumentClassifier{
-	
+	private static final long serialVersionUID = 8216489259978076346L;
+
 	protected ClassifierTrainer<?> getTrainer() {
 		return new NaiveBayesTrainer();
 	}
