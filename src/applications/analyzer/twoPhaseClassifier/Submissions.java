82237516fdd9b6178624c1acf00348108f0aacfd
mariosangiorgio <mariosangiorgio@8dedc91c-0c52-11df-bbc3-97105669a493>
2010-07-12 18:24:54 +0000

    Submissions are grouped by their category

---
+++ src/applications/analyzer/twoPhaseClassifier/Submissions.java
@@ -0,0 +1,36 @@
+package applications.analyzer.twoPhaseClassifier;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Vector;
+
+public class Submissions {
+	private HashMap<String,Collection<String>> submissions;
+	
+	public Submissions(){
+		submissions = new HashMap<String, Collection<String>>();
+	}
+	
+	public void addSubmissionToClass(String className,String submissionName){
+		Collection<String> submissionList;
+		if(submissions.containsKey(className)){
+			submissionList = submissions.get(className);
+		}
+		else{
+			submissionList = new Vector<String>();
+		}
+		submissionList.add(submissionName);
+		submissions.put(className, submissionList);
+	}
+
+	public Set<String> getCategories() {
+		return submissions.keySet();
+	}
+
+	public Collection<String> getPapersOfCategory(String category) {
+		Collection<String> submissionList = new Vector<String>();
+		submissionList.addAll(submissions.get(category));
+		return submissionList;
+	}
+}
