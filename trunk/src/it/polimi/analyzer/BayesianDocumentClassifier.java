2fd4c243ef20d5976fc39db49c33207e07f0a29d
mariosangiorgio <mariosangiorgio@8dedc91c-0c52-11df-bbc3-97105669a493>
2010-08-05 17:14:31 +0000

    Huge refactoring and implementation of the new classifier

---
+++ trunk/src/it/polimi/analyzer/BayesianDocumentClassifier.java
@@ -0,0 +1,17 @@
+package it.polimi.analyzer;
+
+import cc.mallet.classify.ClassifierTrainer;
+import cc.mallet.classify.NaiveBayesTrainer;
+
+public class BayesianDocumentClassifier extends DocumentClassifier{
+	private static final long serialVersionUID = 8216489259978076346L;
+	
+	protected BayesianDocumentClassifier(String pathToTrainingSet) {
+		super(pathToTrainingSet);
+	}
+	
+	protected ClassifierTrainer<?> getTrainer() {
+		return new NaiveBayesTrainer();
+	}
+	
+}
---
+++ trunk/src/it/polimi/bidding/Paper.java
@@ -0,0 +1,21 @@
+package it.polimi.bidding;
+
+public class Paper {
+	private String name;
+	private String content;
+
+	public Paper(String name, String content) {
+		name = name.substring(0, name.length()-4);
+		this.name = name;
+		this.content = name + "\n\n" + content;
+	}
+	
+	public String getName(){
+		return name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
